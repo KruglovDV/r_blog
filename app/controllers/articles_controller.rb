3b121494439e249b44a5066f6c424589854924c1
Dmitry Kruglov <kruglov081289@mail.ru>
2019-11-29 23:21:12 +0300

    Feature: add articles table edit delete links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
   
-  def index() end
+  def index
+    @articles = Article.all
+  end
 
   def new
     @article = Article.new
@@ -18,6 +20,27 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = 'Article wass successfully updated'
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = 'Article was successfully deleted'
+    redirect_to articles_path
   end
 
   private
