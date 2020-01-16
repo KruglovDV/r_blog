e87f1db155b96edf65d8ba89a64b4845f821defb
Dmitry Kruglov <kruglov081289@mail.ru>
2020-01-16 22:11:57 +0300

    Restrict categories actions and correct categories tests

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,6 +3,12 @@ require 'test_helper'
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @category = Category.create(name: 'sports')
+    @user = User.create(
+      username: 'Admin',
+      email: 'email@email.com',
+      password: 'password',
+      admin: true
+    )
   end
 
   test 'should get catigories index' do
@@ -11,6 +17,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should get new' do
+    sign_in_as(@user, 'password')
     get new_category_path
     assert_response :success
   end
@@ -18,5 +25,12 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   test 'should get show' do
     get category_path(@category)
     assert_response :success
+  end
+
+  test 'should redirect create when admin not logged in' do
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: { name: 'new sports' } }
+    end
+    assert_redirected_to login_path
   end
 end
